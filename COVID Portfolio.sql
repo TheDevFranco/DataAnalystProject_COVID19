41163d73d87013664d0c7cdbb4a3054ba7fa2ac3
TheDevFranco <94664155+TheDevFranco@users.noreply.github.com>
2024-02-18 02:10:38 -0600

    Add files via upload

--- COVID Portfolio.sql
+++ COVID Portfolio.sql
@@ -68,3 +68,43 @@ SELECT location, MAX(total_deaths) as TotalDeathCount
 FROM CovidPortfolioProject..CovidDeathtoll
 WHERE continent = '' AND location NOT LIKE '%income%'
 GROUP BY location
+
+
+--Creating queries to run and export as excel documents, to import in Tableau
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidPortfolioProject..CovidDeathtoll
+WHERE continent = '' AND location NOT LIKE '%income%'
+order by 1,2
+
+
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From CovidPortfolioProject..CovidDeathtoll
+WHERE continent = '' AND location NOT LIKE '%income%' 
+and location not in ('World', 'European Union')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidPortfolioProject..CovidDeathtoll
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidPortfolioProject..CovidDeathtoll
+Group by Location, Population, date
+order by PercentPopulationInfected desc
